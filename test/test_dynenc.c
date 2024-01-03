5915a0b7f5c1f95d7da85b910db3c8dc59fc1319
Weizheng Wang <level-128@gmx.com>
2024-01-03 05:16:54 -0500

    alpha dynamic conversion support -- test case

---
+++ test/test_dynenc.c
@@ -0,0 +1,24 @@
+#include "windham_const.h"
+
+void test_dynenc_(char * device_){
+
+//	write_test(device_);
+
+	Key key1;
+	key1.key_or_keyfile_location = "hello world";
+	key1.key_type = EMOBJ_key_file_type_key;
+
+//	action_close(".tmp_windham");
+
+	action_create_convert(device_, DEFAULT_DISK_ENC_MODE, key1, -1, 0, 1, 4096, 4096);
+	
+}
+
+
+
+int test_dynenc(char * device_) {
+	is_skip_conformation = true;
+	print("test dynenc");
+	test_dynenc_(device_);
+	return 0;
+}
