47dc2f20a15db281b305a43e1a1a17dd7c115ce9
level-128 <level-128@gmx.com>
2024-01-31 02:24:59 -0500

    add auto install script

---
+++ auto-install.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# Function to detect the Linux distribution
+detect_distribution () {
+    if grep -q "Debian\|Ubuntu\|Linux Mint" /etc/os-release; then
+        echo "debian"
+    elif grep -q "Fedora\|CentOS\|Red Hat" /etc/os-release; then
+        echo "fedora"
+    elif grep -q "Arch\|Manjaro" /etc/os-release; then
+        echo "arch"
+    fi
+}
+distribution=$(detect_distribution)
+echo "Detected distribution: $distribution"
+echo "Starting package installation..."
+# Redirect stdout to null
+exec > /dev/null
+if [ "$distribution" = "debian" ]; then
+    apt-get update
+    apt-get install -y git cmake gcc libdevmapper-dev libkeyutils-dev libext2fs-dev linux-headers-$(uname -r) libgettextpo-dev libncurses-dev e2fsprogs dosfstools kpartx util-linux
+elif [ "$distribution" = "fedora" ]; then
+    dnf update
+    dnf install -y git cmake gcc device-mapper-devel keyutils-devel libext2fs-devel kernel-devel gettext-runtime gettext-tools ncurses-devel e2fsprogs dosfstools kpartx util-linux
+elif [ "$distribution" = "arch" ]; then
+    pacman -Sy
+    pacman -S --noconfirm git cmake gcc device-mapper keyutils e2fsprogs linux-headers gettext ncurses dosfstools kpartx util-linux
+else
+    exec > /dev/tty
+    echo "Unsupported distribution!"
+    exit 1
+fi
+# Redirect stdout back to the terminal
+exec > /dev/tty
+
+echo "Installation completed. Now cloning the repository..."
+# Clone the git repository
+git clone https://gitlab.com/level-128/windham.git
+cd windham
+
+
+echo "Package installation completed. Now compiling windham..."
+# Navigate to the directory of this script and compile windham
+cmake -B build
+cd build
+make
+sudo make install
+cd ..
+rm -rf build
